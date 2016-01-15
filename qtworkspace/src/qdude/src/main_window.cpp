e03427a42e7fe0dca6c5e9c6f6f50f2ddaf22be8
Peter Tueller <ptueller7@gmail.com>
2016-01-14 19:33:59 -0700

    Bare GUI

--- qtworkspace/src/qdude/src/main_window.cpp
+++ qtworkspace/src/qdude/src/main_window.cpp
@@ -35,21 +35,15 @@ MainWindow::MainWindow(int argc, char** argv, QWidget *parent)
 
     ReadSettings();
 	setWindowIcon(QIcon(":/images/icon.png"));
-	ui.tab_manager->setCurrentIndex(0); // ensure the first tab is showing - qt-designer should have this already hardwired, but often loses it (settings?).
+    //ui.tab_manager->setCurrentIndex(0); // ensure the first tab is showing - qt-designer should have this already hardwired, but often loses it (settings?).
     QObject::connect(&qnode, SIGNAL(rosShutdown()), this, SLOT(close()));
 
 	/*********************
 	** Logging
 	**********************/
-	ui.view_logging->setModel(qnode.loggingModel());
+    //ui.view_logging->setModel(qnode.loggingModel());
     QObject::connect(&qnode, SIGNAL(loggingUpdated()), this, SLOT(updateLoggingView()));
 
-    /*********************
-    ** Auto Start
-    **********************/
-    if ( ui.checkbox_remember_settings->isChecked() ) {
-        on_button_connect_clicked(true);
-    }
 }
 
 MainWindow::~MainWindow() {}
@@ -65,43 +59,6 @@ void MainWindow::showNoMasterMessage() {
     close();
 }
 
-/*
- * These triggers whenever the button is clicked, regardless of whether it
- * is already checked or not.
- */
-
-void MainWindow::on_button_connect_clicked(bool check ) {
-	if ( ui.checkbox_use_environment->isChecked() ) {
-		if ( !qnode.init() ) {
-			showNoMasterMessage();
-		} else {
-			ui.button_connect->setEnabled(false);
-		}
-	} else {
-		if ( ! qnode.init(ui.line_edit_master->text().toStdString(),
-				   ui.line_edit_host->text().toStdString()) ) {
-			showNoMasterMessage();
-		} else {
-			ui.button_connect->setEnabled(false);
-			ui.line_edit_master->setReadOnly(true);
-			ui.line_edit_host->setReadOnly(true);
-			ui.line_edit_topic->setReadOnly(true);
-		}
-	}
-}
-
-
-void MainWindow::on_checkbox_use_environment_stateChanged(int state) {
-	bool enabled;
-	if ( state == 0 ) {
-		enabled = true;
-	} else {
-		enabled = false;
-	}
-	ui.line_edit_master->setEnabled(enabled);
-	ui.line_edit_host->setEnabled(enabled);
-	//ui.line_edit_topic->setEnabled(enabled);
-}
 
 /*****************************************************************************
 ** Implemenation [Slots][manually connected]
@@ -113,7 +70,7 @@ void MainWindow::on_checkbox_use_environment_stateChanged(int state) {
  * the user can always see the latest log message.
  */
 void MainWindow::updateLoggingView() {
-        ui.view_logging->scrollToBottom();
+        //ui.view_logging->scrollToBottom();
 }
 
 /*****************************************************************************
@@ -121,7 +78,7 @@ void MainWindow::updateLoggingView() {
 *****************************************************************************/
 
 void MainWindow::on_actionAbout_triggered() {
-    QMessageBox::about(this, tr("About ..."),tr("<h2>PACKAGE_NAME Test Program 0.10</h2><p>Copyright Yujin Robot</p><p>This package needs an about description.</p>"));
+    QMessageBox::about(this, tr("About ..."),tr("<h2>NASGR Pilot Program Alpha 0.02</h2><p>Copyright Nobody</p><p>I mean, probably we have copyright, who knows...</p><p>This package needs an about description.</p>"));
 }
 
 /*****************************************************************************
@@ -135,29 +92,29 @@ void MainWindow::ReadSettings() {
     QString master_url = settings.value("master_url",QString("http://192.168.1.2:11311/")).toString();
     QString host_url = settings.value("host_url", QString("192.168.1.3")).toString();
     //QString topic_name = settings.value("topic_name", QString("/chatter")).toString();
-    ui.line_edit_master->setText(master_url);
-    ui.line_edit_host->setText(host_url);
+    //ui.line_edit_master->setText(master_url);
+    //ui.line_edit_host->setText(host_url);
     //ui.line_edit_topic->setText(topic_name);
     bool remember = settings.value("remember_settings", false).toBool();
-    ui.checkbox_remember_settings->setChecked(remember);
+    //ui.checkbox_remember_settings->setChecked(remember);
     bool checked = settings.value("use_environment_variables", false).toBool();
-    ui.checkbox_use_environment->setChecked(checked);
+    //ui.checkbox_use_environment->setChecked(checked);
     if ( checked ) {
-    	ui.line_edit_master->setEnabled(false);
-    	ui.line_edit_host->setEnabled(false);
+    //    ui.line_edit_master->setEnabled(false);
+    //    ui.line_edit_host->setEnabled(false);
     	//ui.line_edit_topic->setEnabled(false);
     }
 }
 
 void MainWindow::WriteSettings() {
     QSettings settings("Qt-Ros Package", "qdude");
-    settings.setValue("master_url",ui.line_edit_master->text());
-    settings.setValue("host_url",ui.line_edit_host->text());
+    //settings.setValue("master_url",ui.line_edit_master->text());
+    //settings.setValue("host_url",ui.line_edit_host->text());
     //settings.setValue("topic_name",ui.line_edit_topic->text());
-    settings.setValue("use_environment_variables",QVariant(ui.checkbox_use_environment->isChecked()));
+    //settings.setValue("use_environment_variables",QVariant(ui.checkbox_use_environment->isChecked()));
     settings.setValue("geometry", saveGeometry());
     settings.setValue("windowState", saveState());
-    settings.setValue("remember_settings",QVariant(ui.checkbox_remember_settings->isChecked()));
+    //settings.setValue("remember_settings",QVariant(ui.checkbox_remember_settings->isChecked()));
 
 }
 
